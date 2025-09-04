d90e412d2e72bf797cbbe34856b00daff86bd979
chri6tina <christinashumpert17@icloud.com>
2025-09-04 17:00:18 -0400

    chore: redirect /vending-leads/dc to /vending-leads/washington-dc (301)

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: '/vending-leads/dc',
+        destination: '/vending-leads/washington-dc',
+        permanent: true,
+      },
+      {
         source: '/location/new-york-ny',
         destination: '/vending-leads/new-york',
         permanent: true, // 301 redirect for SEO
